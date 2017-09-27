a492ea4a9eb01a158e619e40054197898aaa2561
javafa <nigne2016@gmail.com>
2017-09-27 12:59:56 +0900

    add background color check clickable add addToBackStack

--- app/src/main/java/com/veryworks/android/fragmentbasic/ListFragment.java
+++ app/src/main/java/com/veryworks/android/fragmentbasic/ListFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -22,8 +23,12 @@ public class ListFragment extends Fragment {
     }
 
     @Override
-    public void onAttach(Context context) {
-        Log.d("Fragment","==========onAttach()");
+    public void onAttach(Context context) { // <- 이 컨텍스트가 나를 삽입한 액티비티이다.
+        // 1. 나를 사용한 액티비티가 내가 제공한 인터페이스를 구현했는지 확인
+        if(context instanceof Callback){
+            callback = (Callback) context; // 2. 구현했다면 인터페이스로 캐스팅해서 사용
+        }
+
         super.onAttach(context);
     }
 
@@ -33,16 +38,26 @@ public class ListFragment extends Fragment {
         super.onDetach();
     }
 
-    // 얘는 액티비티에 부착되면서 동작시작
+    Callback callback = null;
+
+    Button goDetail;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list, container, false);
-
-        // 프래그먼트 화면에 값을 세팅하는 로직은 이 사이에서
-        Log.d("Fragment","==========onCreateView()");
-
+        goDetail = (Button) view.findViewById(R.id.button);
+        goDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callback.goDetail(); // 내가 설계해 놓은 인터페이스를 호출한다
+                                     // 나를 사용하는 측은 이 인터페이스를 강제로 구현해야 한다.
+            }
+        });
         return view;
+    }
+
+    public interface Callback {
+        public void goDetail();
     }
 
     @Override
